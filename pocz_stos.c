4cc82efe543f5df430166320d1eff613d472830c
LukaszKur <lukaszkurasz2@gmail.com>
2018-02-08 15:10:46 +0100

    Operacje na stosie(dane)

--- pocz_stos.c
+++ pocz_stos.c
@@ -1,33 +1,36 @@
-//
-// Created by lukasz on 08.02.18.
-// stos w postaci listy LIFO
-
 #include <stdio.h>
 #include "stos.h"
-int main(){
-    struct  Wezel *top=NULL; //wskaznik stosu
+#include "dane.h"
+
+int main() {
+    struct Wezel *top = NULL; //wskaznik stosu
     struct Dane para;
     struct Dane para_ze_stosu;
-    para.indeks=1;
-    para.wartosc=23;
-    push(&top,para);
 
-    para.indeks=2;
-    para.wartosc=62;
-    push(&top,para);
+    para.indeks = 1;
+    para.wartosc = 23;
+    push(&top, para);
 
-    para.indeks=3;
-    para.wartosc=-35;
-    push(&top,para);
+    para.indeks = 2;
+    para.wartosc = 62;
+    push(&top, para);
 
-    para.indeks=4;
-    para.wartosc=13;
-    push(&top,para);
+    para.indeks = 3;
+    para.wartosc = -35;
+    push(&top, para);
 
-    para_ze_stosu=pop(&top);
-    printf("(%d, %d) zdjety ze stosu\n",para_ze_stosu.indeks,para_ze_stosu.wartosc);
+    para.indeks = 4;
+    para.wartosc = 13;
+    push(&top, para);
 
-    printf("(%d, %d) najmlodszy na stosie\n",peek(top).indeks,peek(top).wartosc);
+    para_ze_stosu = pop(&top);
+    printf("(%d, %d) zdjety ze stosu\n",
+           para_ze_stosu.indeks, para_ze_stosu.wartosc);
 
-    return  0;
+    printf("(%d, %d) najmlodszy na stosie\n",
+           peek(top).indeks, peek(top).wartosc);
+
+    print(top);
+
+    return 0;
 }
